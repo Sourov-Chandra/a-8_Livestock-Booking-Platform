676b152a3e747b09df47c405035105b7f1852dfa
sourov-chandra <sourovtpi@yahoo.com>
2026-05-05 22:34:27 +0600

    try to resolve the deploy issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -7,6 +7,7 @@ await client.connect();
 const db = client.db("livestock-app");
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   secret: process.env.BETTER_AUTH_SECRET,
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
