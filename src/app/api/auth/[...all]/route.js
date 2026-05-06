53fe2130b88b2333006c10f92b6af984a14391a5
sourov-chandra <sourovtpi@yahoo.com>
2026-05-06 08:36:50 +0600

    refactor: move oauth secrets to env vars

--- src/app/api/auth/[...all]/route.js
+++ src/app/api/auth/[...all]/route.js
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+
+
 import { auth } from "@/lib/auth"; // path to your auth file
 import { toNextJsHandler } from "better-auth/next-js";
 
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -7,10 +7,8 @@ await client.connect();
 const db = client.db("livestock-app");
 
 export const auth = betterAuth({
-  baseURL:
-    process.env.BETTER_AUTH_URL ||
-    "https://a-8-livestock-booking-platform-ar5apevru.vercel.app",
-  secret: process.env.BETTER_AUTH_SECRET || "5csic6p6HmAq6a2NbbwbFb9RnpSTOxFt",
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
