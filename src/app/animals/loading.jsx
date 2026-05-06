dd52b93f1d32b4e06b2d503f33928b0fe89578ff
sourov-chandra <sourovtpi@yahoo.com>
2026-05-06 15:03:19 +0600

    added loading for the private route

---
+++ src/app/animals/loading.jsx
@@ -0,0 +1,8 @@
+import Loading from '@/component/sharedLayout/loading/Loading'
+import React from 'react'
+
+const loading = () => {
+  return <Loading />
+}
+
+export default loading
--- src/app/update-profile/loading.jsx
+++ src/app/update-profile/loading.jsx
@@ -2,6 +2,6 @@ import Loading from "@/component/sharedLayout/loading/Loading";
 
 const loading = () => {
   return <Loading />;
-};
+}
 
 export default loading;
