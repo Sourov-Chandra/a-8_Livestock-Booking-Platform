4610f68b7a97a60eb430b5605f8ebe0fc473d588
sourov-chandra <sourovtpi@yahoo.com>
2026-05-06 09:51:27 +0600

    fix: remove await from client.connect for vercel build

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.BETTER_DB_URI);
-await client.connect();
+client.connect();
 const db = client.db("livestock-app");
 
 export const auth = betterAuth({
